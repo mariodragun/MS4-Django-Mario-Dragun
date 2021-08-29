8f6d91f54914f681cc8f8412ceaa6c73b376ffa9
mariodragun <mariodragun1982@gmail.com>
2021-08-29 17:10:18 +0000

    Added basic Questions model, and migrations for it.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=512)
+    description = models.TextField()
+    image = models.ImageField()

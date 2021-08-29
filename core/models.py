f1b1545cd241f7a18c3eb0e00ad3c44b6a70e6e7
mariodragun <mariodragun1982@gmail.com>
2021-08-29 17:21:15 +0000

    Extending Question and Answer models and adding a relationship between them

--- core/models.py
+++ core/models.py
@@ -2,17 +2,34 @@ from django.db import models
 
 
 class Question(models.Model):
+    """Basic Question model.
+    Question has many answers but only one or more of them can be true.
+    """
+
     def __str__(self):
         return self.title
 
     title = models.CharField(max_length=512)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(blank=True, null=True)
+
+    answers = models.ForeignKey("Answer", on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
 
 
 class Answer(models.Model):
+    """Basic Answer model.
+    This model has only the answer text and is_correct field from which admin can define if the
+    Answer is correct or not.
+    """
+
     def __str__(self) -> str:
         return self.answer
 
     answer = models.TextField()
     is_correct = models.BooleanField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)

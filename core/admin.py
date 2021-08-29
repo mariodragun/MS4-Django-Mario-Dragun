a978fe0a457e4c36debd026a0c065bf6a62cd56e
mariodragun <mariodragun1982@gmail.com>
2021-08-29 17:25:21 +0000

    Declaring what to display on admin. Exposing Question and Answer models on the admin CMS

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["title", "created_at"]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["answer", "is_correct"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)

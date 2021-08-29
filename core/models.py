8ba810cdd28fad433749a455aa45b826ca8ae28c
mariodragun <mariodragun1982@gmail.com>
2021-08-29 17:15:26 +0000

    Added basic Answer model, and migrations for it

--- core/models.py
+++ core/models.py
@@ -8,3 +8,11 @@ class Question(models.Model):
     title = models.CharField(max_length=512)
     description = models.TextField()
     image = models.ImageField()
+
+
+class Answer(models.Model):
+    def __str__(self) -> str:
+        return self.answer
+
+    answer = models.TextField()
+    is_correct = models.BooleanField()

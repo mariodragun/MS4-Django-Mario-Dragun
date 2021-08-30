ea24e8c1c84a9eaf4513fa95948bedcca7825948
mariodragun <mariodragun1982@gmail.com>
2021-08-30 16:31:47 +0000

    Changing Admin view to include Tabular InLine view in the Questions Admin.

--- core/admin.py
+++ core/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from .models import Question, Answer
 
 
+class AnswersInline(admin.TabularInline):
+    """Admin inline tabular view to display and be able to easily create multiple Answers to the
+    specific Question Admin view.
+    """
+
+    model = Answer
+
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["title", "created_at"]
-
-
-class AnswerAdmin(admin.ModelAdmin):
-    list_display = ["answer", "is_correct"]
+    inlines = [
+        AnswersInline,
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Answer, AnswerAdmin)

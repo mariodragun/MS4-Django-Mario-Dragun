ec8c05e4d1f03c6b7c6573aca0a65bfde9d840ac
mariodragun <mariodragun1982@gmail.com>
2021-08-30 17:02:38 +0000

    Adding base Quiz model which will store quiz description and other information

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -29,6 +30,27 @@ class Answer(models.Model):
     question = models.ForeignKey("Question", on_delete=models.CASCADE)
     answer = models.TextField()
     is_correct = models.BooleanField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+
+class Quiz(models.Model):
+    """Base Quiz model. Staff member or some other subscription user will be able to create a new quiz."""
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=512)
+    description = models.TextField()
+
+    score = models.IntegerField(default=0)
+
+    published = models.DateTimeField(blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+
+    valid_from = models.DateTimeField(blank=True, null=True)
+    valid_to = models.DateTimeField(blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
---
+++ readme.md
@@ -0,0 +1,1 @@
+### Ms4 -project

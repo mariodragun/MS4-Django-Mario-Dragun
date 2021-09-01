2fe1938e703c32d72a817fbb0c41237492e5adcf
mariodragun <mariodragun1982@gmail.com>
2021-09-01 13:39:42 +0000

    Adding first basic template for quiz, and loading it into the quiz views.

--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Quiz
+from django.template import loader
 
 
 def quiz(request, id):
+    # load correct template
+    template = loader.get_template("core/quiz.html")
 
     try:
+        # try and find quiz object based on the provided `id`
         quiz_object = Quiz.objects.get(id=id, is_active=True)
+        # define context to be sent to template
+        context = {"quiz_object": quiz_object}
     except Exception:
         return HttpResponse("Quiz not found or not active")
-    return HttpResponse("Quiz found - not rendering it yet")
+
+    return HttpResponse(template.render(context, request))

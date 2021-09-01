c40fb77b10590e2171d74501e4d0f54ea296e0a1
mariodragun <mariodragun1982@gmail.com>
2021-09-01 21:43:12 +0000

    hanging routest to better identify views

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from .views import quiz, quiz_question, index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("<int:id>/", quiz, name="index"),
-    path("question/<int:id>/", quiz_question, name="quiz_question"),
+    path("quiz/<int:id>/", quiz, name="quiz_start"),
+    path("quiz/question/<int:id>/", quiz_question, name="quiz_question"),
 ]

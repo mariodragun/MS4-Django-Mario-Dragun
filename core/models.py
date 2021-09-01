25b41bbd4c042dcdb331035887568de429f54880
mariodragun <mariodragun1982@gmail.com>
2021-09-01 14:49:20 +0000

    Adding a model to store users answers

--- core/models.py
+++ core/models.py
@@ -81,3 +81,21 @@ class QuizTaken(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+
+class SelectedAnswer(models.Model):
+    """Model for storing users answers based on the question and the quiz
+    itslef.
+    """
+
+    def __str__(self):
+        return self.content
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey("Quiz", on_delete=models.CASCADE)
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+
+    content = models.CharField(max_length=256)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)

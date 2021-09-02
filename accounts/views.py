ff850aaed118c4fa6fc7f823b2639bea6c9e4973
mariodragun <mariodragun1982@gmail.com>
2021-09-02 22:13:01 +0000

    Register view added along with templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def user_register(request):
+    register_template = loader.get_template("accounts/register.html")
+
+    error_message = None
+
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["req-username-input"]
+        password = request.POST["req-password-input"]
+        repeat_password = request.POST["req-re-password-input"]
+
+        if password != repeat_password:
+            error_message = "Missmatch in the password. Both passwords need to be the same"
+
+            context = {"error_message": error_message}
+            return HttpResponse(register_template.render(context, request))
+
+        user = authenticate(username=username, password=password)
+        login(request, user)
+
+        return redirect("quiz:index")
+
+    context = {"error_message": error_message}
+    return HttpResponse(register_template.render(context, request))

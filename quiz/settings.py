8ac390074f13048ac1efcd331e158d2ddd8a14b0
mariodragun <mariodragun1982@gmail.com>
2021-10-01 17:51:36 +0000

    Changes in the settings because of the architecture change to be able to connect with the apps

--- quiz/settings.py
+++ quiz/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
-    "accounts",
+    "apps.core",
+    "apps.accounts",
 ]
 
 MIDDLEWARE = [

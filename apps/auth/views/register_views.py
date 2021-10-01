1dd029d9311233f8610584b3e25dc2dc2d1ba319
mariodragun <mariodragun1982@gmail.com>
2021-10-01 18:21:29 +0000

    Moving register view from the accounts app into the auth app

---
+++ apps/auth/views/register_views.py
@@ -0,0 +1,29 @@
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+
+def user_register(request):
+    register_template = loader.get_template("auth/register.html")
+
+    error_message = None
+
+    if request.method == "POST":
+        username = request.POST["req-username-input"]
+        password = request.POST["req-password-input"]
+        repeat_password = request.POST["req-re-password-input"]
+
+        if password != repeat_password:
+            error_message = "Missmatch in the password. Both passwords need to be the same"
+
+            context = {"error_message": error_message}
+            return HttpResponse(register_template.render(context, request))
+
+        user = authenticate(username=username, password=password)
+        login(request, user)
+
+        return redirect("quiz:index")
+
+    context = {"error_message": error_message}
+    return HttpResponse(register_template.render(context, request))

a90e4d07bce07ce864cdc01a283970fe18b7f4a0
mariodragun <mariodragun1982@gmail.com>
2021-10-03 13:22:37 +0000

    style changes on multiple apps, and setting checkout/success templates

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,11 +3,13 @@ from django.contrib.auth import views as auth_views
 from .views.register_views import UserRegisterView
 
 urlpatterns = [
+    # use django authentication login view
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="authentication/login.html", redirect_authenticated_user=True),
         name="login",
     ),
+    # use django authentaication logout view
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", UserRegisterView.as_view(), name="register"),
 ]

e72cf80601c6cc1a8e6a6ccb657530aae902a8c0
mariodragun <mariodragun1982@gmail.com>
2021-10-04 11:10:55 +0000

    Changing location of the 404 html template

--- quiz/views.py
+++ quiz/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Defining custom 404 page which will be displayed when DEBUG is set to False (Production environment)
 def error_404(request, exception):
-    return render(request, "404.html", status=404)
+    return render(request, "common/errors/http_404.html", status=404)

7d1c3308f7452be9db073b75490b36bcad2a06d3
mariodragun <mariodragun1982@gmail.com>
2021-10-04 11:05:12 +0000

    Moving success to be visible only for authenticated users

--- apps/payments/views/success_views.py
+++ apps/payments/views/success_views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required, name="dispatch")
 class PaymentSuccessView(TemplateView):
     template_name = "payments/success.html"
